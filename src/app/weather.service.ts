f6115137a923caed664e32524466f87fdaf40ea9
runerune <guildbot@bezkr.es>
2020-07-23 22:27:38 +0200

    add weather download service

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class WeatherService {
+	apiUrl: string;
 
-	constructor() { }
+	constructor(
+		private http: HttpClient,
+	) { 
+		this.apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=be4ebdde0ecfbbba104aa4b05fc031f2&units=metric';
+	}
+
+	getFor(name: string): Observable<object> {
+		return this.http.get(this.apiUrl.replace('%s', name));
+	}
 }
